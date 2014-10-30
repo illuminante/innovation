7b01a8b025513cea229e2105d3ebd1b890e55ac9
illuminante <rafael.rincon@illuminante.org.br>
2014-10-30 17:00:43 -0200

    v0.2

--- js/my-app.js
+++ js/my-app.js
@@ -10,7 +10,46 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+// Conversation flag
+var conversationStarted = false;
 
+// Handle message
+$$('.messagebar .link').on('click', function () {
+    var textarea = $$('.messagebar textarea');
+    // Message text
+    var messageText = textarea.val().trim();
+    // Exit if empy message
+    if (messageText.length === 0) return;
+
+    // Empty textarea
+    textarea.val('').trigger('change');
+
+    // Random message type
+    var messageType = (['sent', 'received'])[Math.round(Math.random())];
+
+    // Avatar and name for received message
+    var avatar, name;
+    if (messageType === 'received') {
+        avatar = 'http://lorempixel.com/output/people-q-c-100-100-9.jpg';
+        name = 'Kate';
+    }
+    // Add message
+    myApp.addMessage({
+        // Message text
+        text: messageText,
+        // Random message type
+        type: messageType,
+        // Avatar and name:
+        avatar: avatar,
+        name: name,
+        // Day
+        day: !conversationStarted ? 'Today' : false,
+        time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
+    })
+
+    // Update conversation flag
+    conversationStarted = true;
+});
 
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
@@ -22,8 +61,9 @@ myApp.onPageInit('about', function (page) {
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
+
 function createContentPage() {
-	mainView.router.loadContent(
+    mainView.router.loadContent(
         '<!-- Top Navbar-->' +
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
@@ -46,5 +86,5 @@ function createContentPage() {
         '  </div>' +
         '</div>'
     );
-	return;
+    return;
 }
