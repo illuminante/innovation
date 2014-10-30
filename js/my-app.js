024ed5acd683bc0312623baa86a283df6ee714ea
illuminante <rafael.rincon@illuminante.org.br>
2014-10-30 19:46:48 -0200

    0.1318

--- js/my-app.js
+++ js/my-app.js
@@ -25,11 +25,11 @@ function gomessage() {
     textarea.val('').trigger('change');
 
     // Random message type
-    var messageType = (['sent', 'received'])[Math.round(Math.random())];
+    var messageType = 'sent'; //(['sent', 'received'])[Math.round(Math.random())];
 
     // Avatar and name for received message
     var avatar, name;
-    if (messageType === 'received') {
+    if (messageType === 'sent') {
         avatar = 'http://lorempixel.com/output/people-q-c-100-100-9.jpg';
         name = 'Kate';
     }
@@ -48,43 +48,7 @@ function gomessage() {
     })
 }
 
-// Handle message
-$$('.messagebar .button').on('click', function () {
-    var textarea = $$('.messagebar textarea');
-    // Message text
-    var messageText = textarea.val().trim();
-    // Exit if empy message
-    if (messageText.length === 0) return;
 
-    // Empty textarea
-    textarea.val('').trigger('change');
-
-    // Random message type
-    var messageType = (['sent', 'received'])[Math.round(Math.random())];
-
-    // Avatar and name for received message
-    var avatar, name;
-    if (messageType === 'received') {
-        avatar = 'http://lorempixel.com/output/people-q-c-100-100-9.jpg';
-        name = 'Kate';
-    }
-    // Add message
-    myApp.addMessage({
-        // Message text
-        text: messageText,
-        // Random message type
-        type: messageType,
-        // Avatar and name:
-        avatar: avatar,
-        name: name,
-        // Day
-        day: !conversationStarted ? 'Today' : false,
-        time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
-    })
-
-    // Update conversation flag
-    conversationStarted = true;
-});
 
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
