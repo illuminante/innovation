d141d708673c6c772f01a3b64a20fd7bc7643cfb
illuminante <rafael.rincon@illuminante.org.br>
2014-11-04 16:59:37 -0200

    0.15

--- js/my-app.js
+++ js/my-app.js
@@ -7,7 +7,8 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    dynamicNavbar: true,
+    fastClicks: false
 });
 
 // Conversation flag
