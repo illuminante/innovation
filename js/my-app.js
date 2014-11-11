20c4da01b4a18db91beb633519b91fddcccec576
illuminante <rafael.rincon@illuminante.org.br>
2014-11-11 16:20:08 -0200

    0.1902

--- js/my-app.js
+++ js/my-app.js
@@ -166,78 +166,48 @@ function sendpass() {
 
 }
 
+
 //############################################SOCIAL
-var googleapi = {
-    authorize: function (options) {
-        var deferred = $.Deferred();
-
-        //Build the OAuth consent page URL
-        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-            client_id: options.client_id,
-            redirect_uri: options.redirect_uri,
-            response_type: 'code',
-            scope: options.scope
-        });
-
-        //Open the OAuth consent page in the InAppBrowser
-        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
-
-        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
-        //which sets the authorization code in the browser's title. However, we can't
-        //access the title of the InAppBrowser.
-        //
-        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
-        //authorization code will get set in the url. We can access the url in the
-        //loadstart and loadstop events. So if we bind the loadstart event, we can
-        //find the authorization code and close the InAppBrowser after the user
-        //has granted us access to their data.
-        $$(authWindow).on('loadstart', function (e) {
-            var url = e.originalEvent.url;
-            var code = /\?code=(.+)$/.exec(url);
-            var error = /\?error=(.+)$/.exec(url);
-
-            if (code || error) {
-                //Always close the browser when match is found
-                authWindow.close();
-            }
-
-            if (code) {
-                //Exchange the authorization code for an access token
-                $$.post('https://accounts.google.com/o/oauth2/token', {
-                    code: code[1],
-                    client_id: options.client_id,
-                    client_secret: options.client_secret,
-                    redirect_uri: options.redirect_uri,
-                    grant_type: 'authorization_code'
-                }).done(function (data) {
-                    deferred.resolve(data);
-                }).fail(function (response) {
-                    deferred.reject(response.responseJSON);
-                });
-            } else if (error) {
-                //The user denied access to the app
-                deferred.reject({
-                    error: error[1]
-                });
-            }
-        });
-
-        return deferred.promise();
-    }
-};
 
 $$(document).on('deviceready', function () {
+    OAuth.initialize('VASu5JaUudJ0zG2ar-1j1duUYso');
 
-    $('#Glogin').on('click', function () {
-        googleapi.authorize({
-            client_id: '339237216668-2aq99gfk8s2g3m646tqbu4nmo2e7s5kj.apps.googleusercontent.com',
-            client_secret: 'WDJTpwi25MPvLGifzdu9vrIs',
-            redirect_uri: 'http://localhost',
-            scope: 'https://www.googleapis.com/auth/analytics.readonly'
-        }).done(function (data) {
-            myApp.alert('Access Token: ' + data.access_token);
-        }).fail(function (data) {
-            myApp.alert(data.error);
-        });
+    $$('#fb-connect').on('touchstart', function () {
+        $$('#result').html("");
+
+        OAuth.popup('facebook')
+            .done(function (r) {
+                // the access_token is available via r.access_token
+                // but the http functions automagically wrap the jquery calls
+                r.get('/me')
+                    .done(function (data) {
+                        $$('#result').html("facebook: Hello, " + data.name + " !");
+                    })
+                    .fail(function (jqXHR, textStatus, errorThrown) {
+                        $$('#result').html("req error: " + textStatus);
+                    });
+            })
+            .fail(function (e) {
+                $$('#result').html('error: ' + e.message);
+            });
+    });
+
+    $$('#tw-connect').on('touchstart', function () {
+        $$('#result').html("");
+        OAuth.popup('twitter')
+            .done(function (r) {
+                // the access_token is available via r.access_token
+                // but the http functions automagically wrap the jquery calls
+                r.get('/1.1/account/verify_credentials.json')
+                    .done(function (data) {
+                        $$('#result').html("twitter: Hello, " + data.name + " !");
+                    })
+                    .fail(function (jqXHR, textStatus, errorThrown) {
+                        $$('#result').html("req error: " + textStatus);
+                    });
+            })
+            .fail(function (e) {
+                $$('#result').html('error: ' + e.message);
+            });
     });
 });
