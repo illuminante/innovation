84b20dd7faeef792f7d75e58df8b8831da9ba5a2
rafusfx <rafael-rincon@outlook.com>
2014-11-12 22:12:21 -0200

    0.1933

--- js/my-app.js
+++ js/my-app.js
@@ -62,16 +62,7 @@ function gomessage() {
 
 
 
-function getCookie(userID) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-    }
-    return "";
-}
+
 
 //Callbacks to run specific code for specific pages, for example for About page//
 myApp.onPageInit('login', function (page) {
@@ -237,7 +228,16 @@ function sendpass() {
 
 //############################################NAVIGATION
 
-
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
 
 //############################################SOCIAL
 
